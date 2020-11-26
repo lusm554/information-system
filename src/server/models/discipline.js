87d6d03535d8d4961df7d2333660c157def68cc9
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-27 01:37:01 +0300

    discipline model

---
+++ src/server/models/discipline.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const DisciplineSchema = new Schema(
+  {
+    "Код дисциплины": { type: Number },
+    "Название": { type: String, required: true, maxlength: 20 }
+  }
+)
+
+DisciplineSchema.pre('save', async function(next) {
+  const currentDate = new Date()
+  this.updatedAt = currentDate
+  
+  this["Код дисциплины"] = await DisciplineModel.countDocuments() + 1
+  next()
+})
+
+DisciplineSchema.statics.findByCode = function(code) {
+  return this.findOne({ "Код дисциплины": code })
+}
+
+DisciplineSchema.statics.findByCodeAndDelete = function(code) {
+  return this.findOneAndDelete({ "Код дисциплины": code })
+}
+
+const DisciplineModel = mongoose.model("Код дисциплины", DisciplineSchema)
+exports.DisciplineModel = DisciplineModel
