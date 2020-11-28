ba1bccb3a83817b2da4e9ae6e7b3f216a2fc4854
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-29 02:10:27 +0300

    class discipline

--- src/server/controllers/discipline.js
+++ src/server/controllers/discipline.js
@@ -1,77 +1,10 @@
-const mongoose = require('mongoose')
-const { DisciplineModel } = require('../models/discipline')
+const { DisciplineModel } = require('../models/discipline')
+const { Model } = require('./index')
 
-class Discipline {
-  constructor(name, res) {
-    this.name = name
-    this.res = res
+class Discipline extends Model {
+  constructor(model) {
+    super(model)
   }
-
-  save() {
-    new DisciplineModel( {"Название": this.name} ).save()
-      .then((doc) => {
-        this.res.json(doc)
-      })
-      .catch((err) => {
-        if (err instanceof mongoose.Error.ValidationError) {
-          this.res.status(400).send('The name of the discipline should be no more than 20 characters.')
-        } else {
-          this.res.status(500).send('An error occurred while saving discipline.')
-          console.log(err)
-        }
-      })
-  }
-
-  static get(id, res, bycode = false) {
-    if (bycode) {
-      handleGetReq(DisciplineModel.findByCode(id), res)
-      return;
-    }
-    handleGetReq(DisciplineModel.findById(id), res)
-  }
-
-  static getAll(res) {
-    handleGetReq(DisciplineModel.find({}), res)
-  }
-
-  static remove(id, res, bycode = false) {
-    if (bycode) {
-      handleDelReq(DisciplineModel.findByCodeAndDelete(id), res)
-      return;
-    }
-    handleDelReq(DisciplineModel.findByIdAndDelete(id), res)
-  } 
 }
 
-function handleGetReq(method, res) {
-  method.then((doc) => {
-    if (doc===null) {
-      res.status(400).send('Document not found.')
-      return;
-    }
-    res.json(doc)
-  })
-  method.catch((error) => {
-    if (error instanceof mongoose.Error.CastError) {
-      res.status(400).send('Document not found.')
-    } else {
-      console.log(error)
-      res.status(500).send('An error occurred while obtaining discipline.')
-    }
-  })
-}
-
-function handleDelReq(method, res) {
-  method
-    .then(() => res.sendStatus(200))
-    .catch((error) => {
-      if (error instanceof mongoose.Error.CastError) {
-        res.status(400).send('Document not found.')
-      } else {
-        console.log(error)
-        res.status(500).send('An error occurred while deleting document.')
-      }
-    })
-}
-
-exports.Discipline = Discipline
+exports.Discipline = new Discipline(DisciplineModel)
