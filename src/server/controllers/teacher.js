f0bf3f7d8dede0e66ce1fcef0606b5b76603137e
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-29 08:14:28 +0300

    teacher controller

---
+++ src/server/controllers/teacher.js
@@ -0,0 +1,10 @@
+const { TeacherModel } = require('../models/teacher')
+const { Model } = require('./index')
+
+class Teacher extends Model {
+	constructor(model) {
+		super(model);
+	}
+}
+
+exports.Teacher = new Teacher(TeacherModel)
