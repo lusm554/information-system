5540bbc0d31dc61fdabc7f0f8ee75ae7bfb89753
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-29 09:25:17 +0300

    student router

---
+++ src/server/routes/studentRouter.js
@@ -0,0 +1,29 @@
+const Router = require('express').Router()
+const { Student } = require('../controllers/student')
+
+Router.get('/', (req, res) => {
+	Student.getAll(res)
+})
+
+Router.get('/:id', (req, res) => {
+	const { id } = req.params
+	Student.get(id, res, {})
+})
+
+Router.post('/new', (req, res) => {
+	const { data } = req.body
+	Student.save(data, res)
+})
+
+Router.put('/:id', (req, res) => {
+	const { id } = req.params
+	const { field } = req.body
+	Student.change(id, res, field)
+})
+
+Router.delete('/:id', (req, res) => {
+	const { id } = req.params
+	Student.delete(id, res, {})
+})
+
+exports.studentRouter = Router
