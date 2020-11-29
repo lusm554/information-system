6f329f2126577f90601a552c7d0a6d0f18935603
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-29 06:15:05 +0300

    router for group

---
+++ src/server/routes/groupRouter.js
@@ -0,0 +1,48 @@
+const Router = require('express').Router()
+const { Group } = require('../controllers/group')
+
+Router.get('/', (req, res) => {
+	Group.getAll(res)
+})
+
+Router.get('/:id', (req, res) => {
+	const { id } = req.params
+	Group.get(id, res, {})
+})
+
+Router.post('/new', (req, res) => {
+	const {
+		code,
+		number,
+		date_created,
+		form_of_study,
+		price,
+		is_study_over
+	} = req.body
+
+	Group.save(
+		{
+			'Код группы': code,
+			'Номер группы': number,
+			'Дата образования': date_created,
+			'Форма обучения': form_of_study,
+			'Плата за семестр': price,
+			'Обучение закончено': is_study_over
+		},
+		res
+	)
+})
+
+Router.put('/:id', (req, res) => {
+	const { id } = req.params
+	const { field } = req.body
+
+	Group.change(id, res, field)
+})
+
+Router.delete('/:id', (req, res) => {
+	const { id } = req.params
+	Group.delete(id, res, {})
+})
+
+exports.GroupRouter = Router
