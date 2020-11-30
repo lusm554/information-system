a7c52325187a49398af22317f4b87a9d4e4fcebb
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-30 23:55:04 +0300

    tabs

--- src/server/routes/assessmentRouter.js
+++ src/server/routes/assessmentRouter.js
@@ -2,28 +2,28 @@ const Router = require('express').Router()
 const { Assessment } = require('../controllers/assessment')
 
 Router.get('/', (req, res) => {
-	Assessment.getAll(res)
+  Assessment.getAll(res)
 })
 
 Router.get('/:id', (req, res) => {
-	const { id } = req.params
-	Assessment.get(id, res, { populate: { populate_by: ['Код_студента', 'Код_ПД'] } })
+  const { id } = req.params
+  Assessment.get(id, res, { populate: { populate_by: ['Код_студента', 'Код_ПД'] } })
 })
 
 Router.post('/new', (req, res) => {
-	const { data } = req.body
-	Assessment.save(data, res)
+  const { data } = req.body
+  Assessment.save(data, res)
 })
 
 Router.put('/:id', (req, res) => {
-	const { id } = req.params
-	const { field } = req.body
-	Assessment.change(id, res, field)
+  const { id } = req.params
+  const { field } = req.body
+  Assessment.change(id, res, field)
 })
 
 Router.delete('/:id', (req, res) => {
-	const { id } = req.params
-	Assessment.delete(id, res, { by_code })
+  const { id } = req.params
+  Assessment.delete(id, res, { by_code })
 })
 
 exports.assessmentRouter = Router
