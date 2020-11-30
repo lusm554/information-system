74d570912e017b75b84d52a6df2cf5ce90d3b0c6
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-30 23:51:39 +0300

    tabs

--- src/server/routes/additional_information_router.js
+++ src/server/routes/additional_information_router.js
@@ -2,28 +2,28 @@ const Router = require('express').Router()
 const { AdditionalInformation } = require('../controllers/additional_information')
 
 Router.get('/', (req, res) => {
-	AdditionalInformation.getAll(res)
+  AdditionalInformation.getAll(res)
 })
 
 Router.get('/:id', (req, res) => {
-	const { id } = req.params
-	AdditionalInformation.get(id, res, { populate: { populate_by: 'Код_студента' } })
+  const { id } = req.params
+  AdditionalInformation.get(id, res, { populate: { populate_by: 'Код_студента' } })
 })
 
 Router.post('/new', (req, res) => {
-	const { data } = req.body
-	AdditionalInformation.save(data, res)
+  const { data } = req.body
+  AdditionalInformation.save(data, res)
 })
 
 Router.put('/:id', (req, res) => {
-	const { id } = req.params
-	const { field } = req.body
-	AdditionalInformation.change(id, res, field)
+  const { id } = req.params
+  const { field } = req.body
+  AdditionalInformation.change(id, res, field)
 })
 
 Router.delete('/:id', (req, res) => {
-	const { id } = req.params
-	AdditionalInformation.delete(id, res, {})
+  const { id } = req.params
+  AdditionalInformation.delete(id, res, {})
 })
 
 exports.AdditionalInformationRouter = Router
